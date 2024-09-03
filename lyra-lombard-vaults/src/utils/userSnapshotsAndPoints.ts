dc3841c5395073cceba06c4e9b2f29ada22bb4e4
Joshua <joshua0702@gmail.com>
2024-09-03 11:43:36 -0700

    feat: initial lombard integration

---
+++ lyra-lombard-vaults/src/utils/userSnapshotsAndPoints.ts
@@ -0,0 +1,77 @@
+import { EthContext, isNullAddress } from "@sentio/sdk/eth";
+import { erc20 } from "@sentio/sdk/eth/builtin";
+import { LyraVaultUserSnapshot } from "../schema/store.js";
+import { EIGENLAYER_POINTS_PER_DAY, ETHERFI_POINTS_PER_DAY, LYRA_VAULTS, MILLISECONDS_PER_DAY } from "../config.js";
+import { toUnderlyingBalance } from "./vaultTokenPrice.js";
+import { getAddress } from "ethers";
+
+export async function updateUserSnapshotAndEmitPointUpdate(ctx: EthContext, vaultName: string, vaultTokenAddress: string, owner: string) {
+    let [oldSnapshot, newSnapshot] = await updateLyraVaultUserSnapshot(ctx, vaultName, vaultTokenAddress, owner)
+    emitUserPointUpdate(ctx, oldSnapshot, newSnapshot)
+}
+
+export async function updateLyraVaultUserSnapshot(ctx: EthContext, vaultName: keyof typeof LYRA_VAULTS, vaultTokenAddress: string, owner: string): Promise<[LyraVaultUserSnapshot?, LyraVaultUserSnapshot?]> {
+    vaultTokenAddress = getAddress(vaultTokenAddress)
+
+    if (isNullAddress(owner)) return [undefined, undefined];
+
+    const vaultTokenContractView = erc20.getERC20ContractOnContext(ctx, vaultTokenAddress)
+    let currentTimestampMs = BigInt(ctx.timestamp.getTime())
+    let currentShareBalance = (await vaultTokenContractView.balanceOf(owner)).scaleDown(18)
+    let underlyingBalance = await toUnderlyingBalance(ctx, LYRA_VAULTS[vaultName].lyra, currentShareBalance, currentTimestampMs)
+
+    let lastSnapshot = await ctx.store.get(LyraVaultUserSnapshot, `${owner}-${vaultTokenAddress}`)
+
+    if (lastSnapshot) {
+        // deep clone to avoid mutation
+        lastSnapshot = new LyraVaultUserSnapshot({
+            id: lastSnapshot.id,
+            owner: lastSnapshot.owner,
+            vaultName: lastSnapshot.vaultName,
+            vaultAddress: lastSnapshot.vaultAddress,
+            timestampMs: lastSnapshot.timestampMs,
+            vaultBalance: lastSnapshot.vaultBalance,
+            underlyingEffectiveBalance: lastSnapshot.underlyingEffectiveBalance
+        })
+    }
+
+    let newSnapshot = new LyraVaultUserSnapshot(
+        {
+            id: `${owner}-${vaultTokenAddress}`,
+            owner: owner,
+            vaultName: vaultName,
+            vaultAddress: vaultTokenAddress,
+            timestampMs: currentTimestampMs,
+            vaultBalance: currentShareBalance,
+            underlyingEffectiveBalance: underlyingBalance
+        }
+    )
+
+    await ctx.store.upsert(newSnapshot)
+
+    return [lastSnapshot, newSnapshot]
+}
+
+export function emitUserPointUpdate(ctx: EthContext, lastSnapshot: LyraVaultUserSnapshot | undefined, newSnapshot: LyraVaultUserSnapshot | undefined) {
+    if (!lastSnapshot || !newSnapshot) return;
+
+    if (lastSnapshot.vaultBalance.isZero()) return;
+
+    const elapsedDays = (Number(newSnapshot.timestampMs) - Number(lastSnapshot.timestampMs)) / MILLISECONDS_PER_DAY
+    const earnedEtherfiPoints = elapsedDays * ETHERFI_POINTS_PER_DAY * lastSnapshot.underlyingEffectiveBalance.toNumber()
+    const earnedEigenlayerPoints = elapsedDays * EIGENLAYER_POINTS_PER_DAY * lastSnapshot.underlyingEffectiveBalance.toNumber()
+    ctx.eventLogger.emit("point_update", {
+        account: lastSnapshot.owner,
+        vaultAddress: lastSnapshot.vaultAddress,
+        earnedEtherfiPoints: earnedEtherfiPoints,
+        earnedEigenlayerPoints: earnedEigenlayerPoints,
+        // last snapshot
+        lastTimestampMs: lastSnapshot.timestampMs,
+        lastVaultBalance: lastSnapshot.vaultBalance,
+        lastunderlyingEffectiveBalance: lastSnapshot.underlyingEffectiveBalance,
+        // new snapshot
+        newTimestampMs: newSnapshot.timestampMs,
+        newVaultBalance: newSnapshot.vaultBalance,
+        newunderlyingEffectiveBalance: newSnapshot.underlyingEffectiveBalance,
+    });
+}
---
+++ lyra-lombard-vaults/src/utils/vaultTokenPrice.ts
@@ -0,0 +1,79 @@
+import { EthChainId, EthContext, getProvider } from "@sentio/sdk/eth"
+import { LyraVaultTokenPrice } from "../schema/store.js"
+import { getLyraVaultTokenContract, getLyraVaultTokenContractOnContext } from "../types/eth/lyravaulttoken.js"
+import { MILLISECONDS_PER_DAY } from "../config.js"
+import { BigDecimal } from "@sentio/sdk"
+import { getAddress } from "ethers"
+import { estimateBlockNumberAtDate } from "./crosschainBlocks.js"
+
+
+export async function saveCurrentVaultTokenPrice(ctx: EthContext, vaultTokenAddress: string, predepositUpgradeTimestampMs: number | undefined) {
+    const nowMs = ctx.timestamp.getTime()
+    const nowMsBigInt = BigInt(nowMs)
+    vaultTokenAddress = getAddress(vaultTokenAddress)
+
+    // Skip saving if Pre-Deposit Upgrade not yet enabled
+    if (predepositUpgradeTimestampMs && nowMsBigInt < BigInt(predepositUpgradeTimestampMs)) {
+        // console.log(`Skipping token price save at time ${nowMsBigInt} for ${vaultTokenAddress} as it's before pre-deposit upgrade`)
+        return
+    } else {
+        console.log(`${vaultTokenAddress}, ${nowMsBigInt}, ${predepositUpgradeTimestampMs}`)
+    }
+
+    // This is taken exclusively from the Lyra Chain
+    const vaultTokenContract = getLyraVaultTokenContract(EthChainId.BITLAYER, vaultTokenAddress)
+    try {
+        const lyraProvider = getProvider(EthChainId.BITLAYER)
+        const lyraBlock = await estimateBlockNumberAtDate(lyraProvider, new Date(nowMs))
+        const shareToUnderlying = (await vaultTokenContract.getSharesValue("1000000000000000000", { blockTag: lyraBlock })).scaleDown(18)
+        console.log(`For ${vaultTokenAddress} got ${shareToUnderlying}`)
+        await ctx.store.upsert(new LyraVaultTokenPrice({
+            id: `${vaultTokenAddress}-${nowMsBigInt}`,
+            vaultAddress: vaultTokenAddress,
+            timestampMs: nowMsBigInt,
+            vaultToUnderlying: shareToUnderlying
+        }))
+
+    } catch (e) {
+        console.log(`Error calling getSharesValue for ${vaultTokenAddress} at ${nowMsBigInt}: ${e.message}`)
+        return
+    }
+}
+
+export async function toUnderlyingBalance(ctx: EthContext, vaultAddress: string, vaultBalance: BigDecimal, snapshotTimestampMs: bigint): Promise<BigDecimal> {
+    vaultAddress = getAddress(vaultAddress)
+
+    // Gets closest vault token price +/- 1 day
+    const upperBound = snapshotTimestampMs + BigInt(MILLISECONDS_PER_DAY * 5)
+    const lowerBound = snapshotTimestampMs - BigInt(MILLISECONDS_PER_DAY * 5)
+    const pricesNearby = await ctx.store.list(LyraVaultTokenPrice, [
+        { field: "vaultAddress", op: "=", value: vaultAddress },
+        { field: "timestampMs", op: "<", value: upperBound },
+        { field: "timestampMs", op: ">", value: lowerBound }
+    ])
+
+    console.log(`Looking through prices nearby for vault ${vaultAddress} with length ${pricesNearby.length} at timestamp ${snapshotTimestampMs} with bounds ${lowerBound} and ${upperBound}`)
+    let tokenPriceWithinBounds: LyraVaultTokenPrice | undefined = await _find_closest_snapshot(pricesNearby, snapshotTimestampMs)
+
+    // handle the last batch
+    if (!tokenPriceWithinBounds) {
+        return vaultBalance
+    }
+    console.log(`Found token price within bounds for vault ${vaultAddress}`)
+    return tokenPriceWithinBounds.vaultToUnderlying.multipliedBy(vaultBalance)
+}
+
+async function _find_closest_snapshot(pricesNearby: LyraVaultTokenPrice[], snapshotTimestampMs: bigint): Promise<LyraVaultTokenPrice | undefined> {
+    let tokenPriceWithinBounds: LyraVaultTokenPrice | undefined;
+    let timestampDiff = BigInt(Number.MAX_SAFE_INTEGER)
+    for (const tokenPrice of pricesNearby) {
+        let diff = snapshotTimestampMs - tokenPrice.timestampMs
+        if (diff < 0) diff = -diff
+
+        if (diff < timestampDiff) {
+            timestampDiff = diff
+            tokenPriceWithinBounds = tokenPrice
+        }
+    }
+    return tokenPriceWithinBounds
+}
